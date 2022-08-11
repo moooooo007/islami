31c21c89d7329cf408c0c4d921164fcaf2cf0be5
Mohammed Safaa <ictp.m2010#gmail.com>
2022-08-11 20:22:26 +0200

    Islami

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Islami());
+}
+class Islami extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
